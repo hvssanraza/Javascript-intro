73de86a1a6c80bb9ef330fac9deca83001a7d052
Hassan Raza <razah1532@gmail.com>
2025-02-01 01:02:13 +0500

    Updated

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ console.log('Intro to Js')
 function addNumbers(a,b){
     return a+b;
 }
+function prodNumbers(a,b){
+    return a*b;
+}
 
 let c= addNumbers(12,12)
+let d = prodNumbers(12,12)
 console.log(c)
+console.log(d)
 
 console.log('My first Js program!')
